db725e83f3bc7699e2d0227ca5a0445fb724603d
Rodrigo Avellar <roavellarm@gmail.com>
2021-04-22 00:17:14 -0300

    Update ?Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
-export default function Home(props) {
-  // SPA - Single Page Application
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/episodes')
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data))
-  // }, [])
+import { GetServerSideProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
+import api from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+
+type Episode = {
+  id: string
+  title: string
+  description: string
+  members: string
+  thumbnail: string
+  url: string
+  duration: number
+  durationAsString: string
+  publishedAt: string
+  // ...
+}
+
+type HomeProps = {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <h1>Index</h1>
@@ -13,24 +30,31 @@ export default function Home(props) {
   )
 }
 
-// SSR - Server Side Rendering
-// export async function getServerSideProps() {
-//   const response = await fetch('http://localhost:3333/episodes')
-//   const data = await response.json()
-//   return {
-//     props: {
-//       episodes: data,
-//     },
-//   }
-// }
+export const getStaticProps: GetServerSideProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  })
 
-// SSG - Server Static Generation
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBr }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url,
+    }
+  })
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }
